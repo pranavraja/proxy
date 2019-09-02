9961e1e4ceff0645cf030953fa4063df8f9067d2
Pranav Raja <rickdangerous1@gmail.com>
2019-09-02 11:22:38 +1000

    map: Allow paths in mappings

--- cmd/proxy/map.go
+++ cmd/proxy/map.go
@@ -4,23 +4,39 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/pranavraja/proxy"
 )
+
+type hostpath struct {
+	Host string
+	Path string
+}
+
+func parseHostpath(s string) hostpath {
+	hp := strings.SplitN(s, "/", 2)
+	m := hostpath{Host: hp[0]}
+	if len(hp) > 1 {
+		m.Path = "/" + hp[1]
+	}
+	return m
+}
 
 func main() {
 	if len(os.Args) <= 1 || len(os.Args)%2 == 0 {
 		log.Fatal("usage: map host1 other host2 other")
 	}
-	mappings := make(map[string]string)
+	mappings := make(map[hostpath]hostpath)
 	for i := 1; i < len(os.Args); i += 2 {
-		mappings[os.Args[i]] = os.Args[i+1]
+		mappings[parseHostpath(os.Args[i])] = parseHostpath(os.Args[i+1])
 	}
 	p := proxy.New(func(r *http.Request) {
-		for host, remote := range mappings {
-			if r.Host == host {
-				r.Host = remote
-				r.URL.Host = remote
+		for og, remote := range mappings {
+			if r.Host == og.Host && strings.HasPrefix(r.URL.Path, og.Path) && !strings.HasPrefix(r.URL.Path, remote.Path) {
+				r.Host = remote.Host
+				r.URL.Host = remote.Host
+				r.URL.Path = remote.Path + strings.TrimPrefix(r.URL.Path, og.Path)
 				r.URL.Scheme = "http"
 			}
 		}
